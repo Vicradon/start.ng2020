c69cb6d395ec276931e849edf4b200fe8c37e414
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-03-25 07:32:36 +0100

    Change code to accept user input
    The previous version only contained a function

--- backend/python-tasks/python-task-1/area-calculator.py
+++ backend/python-tasks/python-task-1/area-calculator.py
@@ -1,6 +1,8 @@
 from math import pi
 
+userInput = float(input("Please enter the radius of the circle "))
+
 def circleArea(radius):
   return radius * radius * pi
 
-print(circleArea(4))
+print(f"The area of the circle with radius {userInput} is {circleArea(userInput)}")
